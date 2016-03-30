1a22c31664b4774cbd3d1e45dbebfb50b18f3259
Matt Crowder <mcrowder65@gmail.com>
2016-03-30 10:34:08 -0600

    react is great, not enoughd documentation though, going with django.

--- app/fix.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import fileinput
-
-with fileinput.FileInput("bundle.js", inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace("/** @jsx React.DOM */", ""), end='')
--- models/express.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-// setup static directory
-app.use(express.static('app'));
-
-module.exports = app;
