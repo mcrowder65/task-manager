bf95a9347aaf76d6e9130be7e60041b36f26dc28
Matt Crowder <mcrowder65@gmail.com>
2016-04-03 10:47:45 -0600

    added a .gitignore and configured my mysql db

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,6 +81,15 @@ DATABASES = {
     }
 }
 
+# settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '../my.cnf',
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -119,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
