a9e318d82687a5720cf2a9abaeb3b5fba471a0d8
Matt Crowder <mcrowder65@gmail.com>
2017-05-26 09:19:40 -0600

    signup fixed

--- client/js/signup.js
+++ client/js/signup.js
@@ -1,26 +1,25 @@
-var app= angular.module('app');
+var app = angular.module('app');
 app.controller('signup', ['$scope', '$http', ($scope, $http) => {
 
-    $scope.signUp = () => {
-      try {
-        $scope.username = $scope.username.toLowerCase();
-        var username = $scope.username;
-        var password = $scope.password;
-        $http({
-            method: 'POST',
-            url: '/signup',
-            data: {username, password}
-        }).then(successCallback = (response) => {
-            localStorage.token = response.data.token;
-            window.location="/index.html";
-        }, errorCallback = (response) => {
-            $scope.openToast('something went wrong!');
-            localStorage.token="";
-            throw new Error("signup busted!");
-        });
-      } catch(error) {
-        $scope.openToast('something went wrong!');
-      }
+  $scope.signUp = async() => {
+    try {
+      $scope.username = $scope.username.toLowerCase();
+      const response = await $http({
+        method: 'POST',
+        url: '/signup',
+        data: {
+          username: $scope.username,
+          password: $scope.password
+        }
+      });
 
+      localStorage.token = response.data;
+      $scope.$emit('callIsLoggedIn', {});
+      window.location = "/#!/allReminders";
+
+    } catch(error) {
+      $scope.openToast('something went wrong!');
     }
+
+  }
 }]);
